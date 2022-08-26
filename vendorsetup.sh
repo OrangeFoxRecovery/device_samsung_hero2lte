0e272543feca69f85a20a59bd819379440048407
Sushrut1101 <guptasushrut@gmail.com>
2022-08-26 18:30:03 +0530

    hero2lte: Update the Build Vars
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,12 +17,12 @@
 FDEVICE="hero2lte"
 
 fox_get_target_device() {
-	local chkdev=$(echo "$BASH_SOURCE" | grep \"$FDEVICE\")
+local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
 	if [ -n "$chkdev" ]; then
 		FOX_BUILD_DEVICE="$FDEVICE"
 	else
-		chkdev=$(set | grep BASH_ARGV | grep \"$FDEVICE\")
-		[ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+		chkdev=$(set | grep BASH_ARGV | grep $FDEVICE)
+	[ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
 	fi
 }
 
@@ -32,54 +32,60 @@ fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
-	# Basic Vars
+	# Base
 	export ALLOW_MISSING_DEPENDENCIES=true
 	export LC_ALL="C"
 	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
-
-	export OF_PATCH_AVB20=1
-	export OF_USE_MAGISKBOOT=1
-	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-	export OF_NO_MIUI_PATCH_WARNING=1
-	export FOX_REPLACE_BUSYBOX_PS=1
-	export OF_USE_NEW_MAGISKBOOT=1
-	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
-	export OF_FBE_METADATA_MOUNT_IGNORE=1
-	export FOX_DELETE_AROMAFM=1
-	export OF_USE_SYSTEM_FINGERPRINT=1
-	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1600876644"; # Wednesday, September 23 2020 19:07:11 BST
-	export FOX_USE_BASH_SHELL=1
-	export FOX_ASH_IS_BASH=1
-	export FOX_USE_NANO_EDITOR=1
-	export FOX_USE_TAR_BINARY=1
-	export FOX_USE_ZIP_BINARY=1
-	export FOX_USE_SED_BINARY=1
-	export FOX_USE_XZ_UTILS=1
-	export OF_USE_GREEN_LED=0
-
-	# OTA for custom ROMs
-	export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-	export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-	export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
-
-	# Quick backup defaults
-	export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;"
-
-	# Exynos ($FDEVICE) Specific
+	
+	# Exynos ($FDEVICE) specific
 	export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/platform/155a0000.ufs/by-name/RECOVERY"
 	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/platform/155a0000.ufs/by-name/SYSTEM"
 	# FOX_RECOVERY_VENDOR_PARTITION is not set as the device is non-treble
 	export FOX_RECOVERY_BOOT_PARTITION="/dev/block/platform/155a0000.ufs/by-name/BOOT"
 	export OF_FL_PATH1="/system/flashlight"
-	export OF_DEVICE_WITHOUT_PERSIST="1"
-
+	export OF_DEVICE_WITHOUT_PERSIST=1
+	export OF_SKIP_FBE_DECRYPTION=1
+	
 	# R11.1 Settings
-	export FOX_R11="1"
 	export FOX_VERSION="R11.1_1"
 	export OF_MAINTAINER="Sushrut1101"
+	export OF_MAINTAINER_AVATAR="misc/Sushrut1101.png"
+	
+	export FOX_ADVANCED_SECURITY="1"
+	export OF_USE_TWRP_SAR_DETECT="1"
+	export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+	export OF_DISABLE_MIUI_OTA_BY_DEFAULT=0
+	export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+	export OF_OTA_BACKUP_STOCK_BOOT_IMAGE=1
 
+	# Quick Backup Defaults
+	export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;"
+	
+	# Common
+	export TARGET_ARCH=arm64
+	export TW_DEFAULT_LANGUAGE="en"
+	export OF_KEEP_FORCED_ENCRYPTION="1"
+	export OF_PATCH_AVB20="1"
+	export OF_USE_MAGISKBOOT="1"
+	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
+	export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
+	export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
+	export FOX_USE_LZMA_COMPRESSION="1"
+	
+	export FOX_USE_BASH_SHELL="1"
+	export FOX_ASH_IS_BASH="1"
+	export FOX_USE_TAR_BINARY="1"
+	export FOX_USE_ZIP_BINARY="1"
+	export FOX_REPLACE_BUSYBOX_PS="1"
+	export OF_USE_NEW_MAGISKBOOT="1"
+	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP="1"
+	
+	# Use the System (ROM) Fingerprint where Available
+	export OF_USE_SYSTEM_FINGERPRINT="1"
+
+	# Run Post Format Process for MTP
+	export OF_RUN_POST_FORMAT_PROCESS=1
+	
 	# Magisk
 	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk.zip
 	if [ -n "${FOX_USE_SPECIFIC_MAGISK_ZIP}" ]; then
@@ -92,11 +98,11 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 		fi
 	fi
 
-	# let's see what are our build VARs
+	# Let's See what are our Build VARs
 	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
 		export | grep "FOX" >> $FOX_BUILD_LOG_FILE
 		export | grep "OF_" >> $FOX_BUILD_LOG_FILE
 		export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
 		export | grep "TW_" >> $FOX_BUILD_LOG_FILE
-	fi
+ 	fi
 fi
